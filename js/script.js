0f9284dbb241bb33c5f9c80c06e5dccb5e1fe29d
jennifergordon <jennifergordon1021@gmail.com>
2022-05-27 23:36:25 -0400

    Added functions to display repos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 // github username
 const username = "jgordon85";
+// repo list
+const repoList = document.querySelector(".repo-list");
 
 const getGithub = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -25,4 +27,20 @@ const displayUserInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     overview.append(div);
+    getRepoList();
+};
+
+const getRepoList = async function () {
+  const repoInfo = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await repoInfo.json();
+  displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
 };
